97cce1a268cb1346a6a96e4ce953591095746434
unknown <adrian@meudev.com.br>
2022-03-15 21:42:14 -0300

    Desafio 02 - Trabalhando com middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,94 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+    user => user.username === username
+  );
+
+  if (!user) {
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro) {
+    if (user.todos.length == 10) {
+      return response.status(403).json({
+        error: ""
+      });
+    };
+  };
+
+  request.user = user;
+
+  return next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const validID = validate(id);
+
+  const user = users.find(
+    user => user.username === username
+  );
+
+  if (!user) {
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  };
+
+  if (!validID) {
+    return response.status(400).json({
+      error: "ID invalid!"
+    });
+  }
+
+  const todo = user.todos.find(
+    todo => todo.id === id
+  );
+
+  if(!todo) {
+    return response.status(404).json({
+      error: "Todo not found!"
+    })
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(
+    user => user.id === id
+  );
+
+  if (!user) {
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
